ea5a53ce22b7ab460cd20e3683e90b004bb4ea71
Farkhatzhan Makhmudov <faralost@gmail.com>
2023-01-19 17:29:22 +0600

    add routing

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function AboutPage () {
+    return (
+        <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid consequuntur corporis est, impedit in ipsa ratione voluptates? Facere impedit, inventore labore molestiae nesciunt odit placeat quae quaerat reiciendis voluptas. Animi consequuntur culpa, doloremque esse iure, minima necessitatibus odio perferendis perspiciatis possimus provident quas quos sit suscipit tenetur, vel veniam veritatis!
+        </div>
+    )
+}
+
